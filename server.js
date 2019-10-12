fccddb564b81a108bce3fb6c4bca809ead03508e
MostafaIn <mostafa.hazareh@integrify.io>
2019-10-12 22:47:31 +0300

    initials

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req,res)=> res.send('API is Running ...'));
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API is Running ...'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
