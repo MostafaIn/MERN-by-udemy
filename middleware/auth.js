f8f672818b8d20c422ecd700e4edc18f31e90182
MostafaIn <mostafa.hazareh@integrify.io>
2019-10-17 22:54:02 +0300

    User API Routes & JWT Authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function(req, res, next) {
-	// get token from header
+module.exports = function (req, res, next) {
+	// Get token from header
 	const token = req.header('x-auth-token');
 
-	// check if not token
+	// Check if not token
 	if (!token) {
-		res.status(401).json({ msg: 'no token, authorization denied!' });
+		return res.status(401).json({ msg: 'No token, authorization denied' });
 	}
 
-	// verify token
+	// Verify token
 	try {
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
-		res.user = decoded.user;
+
+		req.user = decoded.user;
 		next();
 	} catch (err) {
-		res.status(401).json({ msg: 'token is not valid!' });
+		res.status(401).json({ msg: 'Token is not valid' });
 	}
 };
