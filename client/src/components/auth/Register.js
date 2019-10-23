7b248185577673806dd52f38f3dbd6571a70164d
MostafaIn <mostafa.hazareh@integrify.io>
2019-10-23 18:17:16 +0300

    Redux setup and Alerts(like snackbar of Mui)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
 import axios from 'axios';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -14,7 +16,7 @@ const Register = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		if (password !== password2) {
-			console.log('passwords do not match');
+			setAlert('passwords do not match', 'danger');
 		} else {
 			const newUser = {
 				name,
@@ -97,4 +99,7 @@ const Register = () => {
 	);
 };
 
-export default Register;
+export default connect(
+	null,
+	{ setAlert }
+)(Register);
