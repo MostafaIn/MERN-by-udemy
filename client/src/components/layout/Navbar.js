cb6e9432dfc9a31451e547f70605e2951f5bb751
MostafaIn <mostafa.hazareh@integrify.io>
2019-10-24 13:47:19 +0300

    starting on the Dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
+				<Link to="/dashboard">
+					<i className="fas fa-user"></i> Dashboard
+				</Link>
+			</li>
+			<li>
 				<Link onClick={logout} to="/">
 					<i className="fas fa-sign-out-alt"></i>
 					<span className="hide-sm">Log out</span>
@@ -19,7 +24,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 		<ul>
 			<li>
 				<Link to="/">
-					<i className="fas fa-users"></i> Developers
+					<i className="fas fa-users"></i> Users
 				</Link>
 			</li>
 			<li>
---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+
+export default () => (
+	<Fragment>
+		<img src={`https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj8gfvVybTlAhWSwsQBHWrhAJAQjRx6BAgBEAQ&url=https%3A%2F%2Fgfycat.com%2Fprestigiousdisloyalcaiman&psig=AOvVaw0_jJLUdIv1RWtdcX-A7lyJ&ust=1571995014866293`} style={{ width: '200px', margin: 'auto', display: 'block' }} alt="loading..." />
+	</Fragment>
+);
