a99dfdc725f504778f92b6310bee44f6fcac41d5
MostafaIn <mostafa.hazareh@integrify.io>
2019-11-06 14:54:24 +0200

    delete Education,Experiences and Account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,12 +2,13 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
-
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+const Dashboard = ({ getCurrentProfile, deleteAccount, auth: { user }, profile: { profile, loading } }) => {
 	useEffect(() => {
 		getCurrentProfile();
 	}, []);
@@ -22,6 +23,13 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 			{profile !== null ? (
 				<Fragment>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
+					<div className="my-2">
+						<button className="btn btn-danger" onClick={() => deleteAccount()}>
+							<i className="fas fa-user-minus"> Delete my Account</i>
+						</button>
+					</div>
 				</Fragment>
 			) : (
 				<Fragment>
@@ -37,6 +45,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 
 Dashboard.propTypes = {
 	getCurrentProfile: PropTypes.func.isRequired,
+	deleteAccount: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	profile: PropTypes.object.isRequired,
 };
@@ -47,5 +56,5 @@ const mapStateToProps = (state) => ({
 });
 export default connect(
 	mapStateToProps,
-	{ getCurrentProfile }
+	{ getCurrentProfile, deleteAccount }
 )(Dashboard);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const config = require('config');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -134,7 +135,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
 	try {
-		// @todo - remove users posts
+		// remove users posts
+		await Post.deleteMany({user: req.user.id})
 		// remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 
